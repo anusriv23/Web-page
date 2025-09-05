40d28948679fa90f0fd8b162a1951d93c60e17b2
anusriv23 <anusrim447@gmail.com>
2025-09-05 08:22:29 +0530

    Initial commit

---
+++ subi.js
@@ -0,0 +1,24 @@
+function home(){
+    window.location.href='home.html';
+}
+function about(){
+    window.location.href='about.html';
+}
+ let show=document.getElementById("show")
+ function anu1(){
+    let box=document.getElementById("hidden");
+    if(box.style.display==="block"){
+        box.style.display="none";
+    
+    }
+    else{
+        box.style.display="block";
+    }
+}
+function anu(){
+    window.alert("warning");
+}
+const myform=()=>{
+    
+    document.write("name");
+}
